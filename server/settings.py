0599e43a8bb692087cfa2ba44940cad003a4ea28
Connor Carciofini <ccarciofini@gmail.com>
2023-06-05 19:30:29 -0500

    whitenoise

--- server/settings.py
+++ server/settings.py
@@ -124,6 +124,7 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/assets/')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
